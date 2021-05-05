3b4cae4060e3e10aca118b25775ab12c28d25ba9
Bryan Ames <amesb@eou.edu>
2021-05-05 03:04:56 -0700

    fixed some bugs, all functionality working (I think)

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,12 +2,12 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
-from flask.auth import login_required
+from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
-@bp.rout('/')
+@bp.route('/')
 def index():
     db = get_db()
     posts = db.execute(
@@ -45,8 +45,8 @@ def create():
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user ON p.author_id = u.id'
-        ' WHERE p.id = ?'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
         (id,)
     ).fetchone()
 
@@ -77,7 +77,7 @@ def update(id):
             db = get_db()
             db.execute(
                 'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?'
+                ' WHERE id = ?',
                 (title, body, id)
             )
             db.commit()
